23ce9b48b8d6cb85c5916ed03e36114394fcdcd8
Ali Nategh <alinategh005@gmail.com>
2022-08-08 22:54:30 +0430

    Added signup api and a get_members to test access tokens

--- src/script.ts
+++ src/script.ts
@@ -32,6 +32,55 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send("Home Page");
 });
 
+// GET '/admin/get_members' to bring back all the members (JUST FOR TESTING)
+app.get('/admin/get_members', authenticateToken, async (req: Request, res: Response) => {
+    getMembers(res)
+        .catch(e => {
+            console.error(e.message)
+        })
+        .finally(async () => {
+            await prisma.$disconnect()
+        });
+});
+
+// POST '/users/signup' adding a member to the Database
+app.post('/users/signup', async (req: Request, res: Response) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        // Get user info from request body
+        const user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+            age: req.body.age          // Age is optional
+        };
+        // Generate a refreshToken for this user
+        const refresh_token: string = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET as string);
+        // Add user to Database
+        const user_data = await prisma.user.create({   
+            data: {
+                name: user.name,
+                email: user.email,
+                password: hashedPassword,
+                age: user.age,
+                refreshToken: {
+                    create: {
+                        id: refresh_token
+                    }
+                }
+            },
+            include: {refreshToken: true}
+        });
+        console.log(user_data);
+        res.status(201).send();
+    } catch (error){
+        if (String(error).includes('email')){
+            res.status(400).send("Email already exists");
+        }
+        res.status(500).send();
+    };
+});
+
 
 
 // PORT listen
