db362810ec40d7fd9f090dcb8fac16caa707e018
Ali Nategh <alinategh005@gmail.com>
2022-08-13 19:05:58 +0430

    refactor error

--- src/middleware/apiErrorHandler.middleware.ts
+++ src/middleware/apiErrorHandler.middleware.ts
@@ -2,8 +2,8 @@ import ApiError from "../models/ApiError";
 import { Request, Response, NextFunction } from 'express';
 
 export function apiErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    // better use library winston or bunja
-    console.error(err);
+    // // better use library winston or bunja
+    // console.error(err);
 
     if (err instanceof ApiError) {
         res.status(err.code).json(err.message)
