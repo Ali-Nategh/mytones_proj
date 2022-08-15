19643974231d98b4128e4df90379337c147257a7
Ali Nategh <alinategh005@gmail.com>
2022-08-16 00:31:22 +0430

    refactor: clean up code, also added otp validation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,48 +1,28 @@
-import { loginUserService, signUpUserService, logoutUserService, refreshUserTokenService } from "../services/user.service";
+import { loginUserService, signUpUserService, logoutUserService, refreshUserTokenService, verifyEmailService } from "../services/user.service";
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
 
 
 export async function signUpUser(req: Request, res: Response) {
-    // Validate and handle validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     signUpUserService(req, res);
 };
 
 
 export async function loginUser(req: Request, res: Response) {
-    // Validate and handle validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     loginUserService(req, res);
 }
 
 
 export async function logoutUser(req: Request, res: Response) {
-    // Validate and handle validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     logoutUserService(req, res);
 }
 
 
 export async function refreshUserToken(req: Request, res: Response) {
-    // Validate and handle validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     refreshUserTokenService(req, res);
+}
+
+export async function validateUserOtp(req: Request, res: Response) {
+    if (!verifyEmailService(req.body.email, req.body.password, res)) return false;
 }
