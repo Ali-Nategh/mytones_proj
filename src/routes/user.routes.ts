43465c9cc363627fea71ec50912955d2055abaf1
Ali Nategh <alinategh005@gmail.com>
2022-08-16 00:59:12 +0430

    feat: validateEmail route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,21 +3,26 @@ const router = require('express').Router();
 import bodyParser from 'body-parser';
 const urlencodedparser = bodyParser.urlencoded({ extended: false });
 
-import { validateUsername, validateEmail, validatePassword, validateToken } from '../services/validation.service';
-import { signUpUser, loginUser, refreshUserToken, logoutUser } from '../controllers/user.controller';
+import { validateUsername, validateEmail, validatePassword, validateToken, validateOtp } from '../services/validation.service';
+import { signUpUser, loginUser, refreshUserToken, logoutUser, validateUserOtp } from '../controllers/user.controller';
+import validationMiddleware from '../middlewares/validateResults.middleware';
 
 
 
 // POST '/user/signup' adding a member to the Database
-router.post('/signup', urlencodedparser, [validateUsername(), validateEmail(), validatePassword()], signUpUser);
+router.post('/signup', urlencodedparser, [validateUsername(), validateEmail(), validatePassword()], validationMiddleware, signUpUser);
 
 // POST '/user/login'
-router.post('/login', [validateEmail(), validatePassword()], loginUser);
+router.post('/login', [validateEmail(), validatePassword()], validationMiddleware, loginUser);
 
 // POST '/user/refreshtoken'
-router.post('/refreshtoken', [validateToken()], refreshUserToken);
+router.post('/refreshtoken', [validateToken()], validationMiddleware, refreshUserToken);
+
+// POST '/user/validateEmail'
+router.post('/validateEmail', [validateEmail(), validateOtp()], validationMiddleware, validateUserOtp);
 
 // DELETE '/user/logout'
-router.delete('/logout', [validateToken()], logoutUser);
+router.delete('/logout', [validateToken()], validationMiddleware, logoutUser);
+
 
 export default router;
