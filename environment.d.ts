1902097f87c655cae1446ebf457dd58c24e236e6
Ali Nategh <alinategh005@gmail.com>
2022-08-15 23:28:31 +0430

    feat: added mailing service

--- environment.d.ts
+++ environment.d.ts
@@ -5,8 +5,10 @@ declare global {
             ACCESS_TOKEN_SECRET: string | undefined;
             REFRESH_TOKEN_SECRET: string | undefined;
             DATABASE_URL: string;
+            MAIL_EMAIL: string;
+            MAIL_PASSWORD: string;
         }
     }
 }
 // converting it into a module by adding an empty export statement.
-export {}
+export { }
