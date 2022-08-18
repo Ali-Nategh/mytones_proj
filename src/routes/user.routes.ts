b58fcc5f5b4b18fc147bfd49407f7b8695306dd4
Ali Nategh <alinategh005@gmail.com>
2022-08-18 21:30:52 +0430

    feat: added swagger documentation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -117,14 +117,153 @@ router.post('/signup', urlencodedparser, [validateUsername(), validateEmail(), v
 // POST '/user/login'
 router.post('/login', [validateEmail(), validatePassword()], validationMiddleware, loginUser);
 
+/**
+ * @swagger
+ * /user/refreshtoken:
+ *  post:
+ *      tags: [Users]
+ *      summary: Get a new Access Token
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *        - in: body
+ *          name: refreshtoken
+ *          description: Refreshing Access Token
+ *          schema:
+ *              type: object
+ *              required:
+ *                  - token
+ *              properties:
+ *                  token:
+ *                      type: string
+ *      responses:
+ *          '200': 
+ *              description: Access token generated
+ *              content: 
+ *                  apllication/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User' 
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '403':
+ *              description: Password wrong or Information invalid
+ *          '500': 
+ *              description: Something went wrong
+ */
+
 // POST '/user/refreshtoken'
 router.post('/refreshtoken', [validateToken()], validationMiddleware, refreshUserToken);
+
+/**
+ * @swagger
+ * /user/validateEmail:
+ *  post:
+ *      tags: [Users]
+ *      summary: Validate email with the sent otp
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *        - in: body
+ *          name: user
+ *          description: Email validation wih one time password
+ *          schema:
+ *              type: object
+ *              required:
+ *                  - email
+ *                  - otp
+ *              properties:
+ *                  email:
+ *                      type: string
+ *                  otp:
+ *                      type: string
+ *      responses:
+ *          '201': 
+ *              description: User was successfully created
+ *              content: 
+ *                  apllication/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User' 
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '403':
+ *              description: Password wrong or Information invalid
+ *          '500': 
+ *              description: Something went wrong
+ */
 
 // POST '/user/validateEmail'
 router.post('/validateEmail', [validateEmail(), validateOtp()], validationMiddleware, validateUserOtp);
 
+/**
+ * @swagger
+ * /user/resendEmail:
+ *  post:
+ *      tags: [Users]
+ *      summary: Resend the otp email if user hasn't received it
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *        - in: body
+ *          name: user
+ *          description: The user to login
+ *          schema:
+ *              type: object
+ *              required:
+ *                  - email
+ *              properties:
+ *                  email:
+ *                      type: string
+ *      responses:
+ *          '201': 
+ *              description: Email sent successfully
+ *              content: 
+ *                  apllication/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User' 
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '403':
+ *              description: Password wrong or Information invalid
+ *          '500': 
+ *              description: Something went wrong
+ */
+
 // POST '/user/validateEmail'
 router.post('/resendEmail', [validateEmail()], validationMiddleware, resendUserOtp);
+
+/**
+ * @swagger
+ * /user/logout:
+ *  post:
+ *      tags: [Users]
+ *      summary: Log out and Deactivate the Refresh Token
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *        - in: body
+ *          name: refreshtoken
+ *          description: Deactivate Refresh Token
+ *          schema:
+ *              type: object
+ *              required:
+ *                  - token
+ *              properties:
+ *                  token:
+ *                      type: string
+ *      responses:
+ *          '200': 
+ *              description: Logged out successfully
+ *              content: 
+ *                  apllication/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User' 
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '403':
+ *              description: Password wrong or Information invalid
+ *          '500': 
+ *              description: Something went wrong
+ */
 
 // DELETE '/user/logout'
 router.delete('/logout', [validateToken()], validationMiddleware, logoutUser);
