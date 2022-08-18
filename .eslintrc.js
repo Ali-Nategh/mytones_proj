f871b0c984153bafb4c6afe2b727dba5645440fc
Ali Nategh <alinategh005@gmail.com>
2022-08-18 18:19:32 +0430

    refactor: setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'standard-with-typescript',
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'spaced-comment': ['error', 'always'],
+        semi: ['error', 'always'],
+        'semi-spacing': 'error',
+        'no-extra-semi': 'error',
+        'no-unexpected-multiline': 'error',
+        'max-len': ['error', { code: 80 }],
+        'comma-style': ['error', 'last'],
+        'comma-dangle': ['error', 'always-multiline'],
+        indent: ['error', 4],
+        'space-infix-ops': 'error',
+        'brace-style': 'error',
+        'space-before-blocks': 'error',
+        'newline-per-chained-call': 'error',
+        'space-in-parens': ['error', 'never'],
+        'array-bracket-spacing': ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'comma-spacing': ['error', { before: false, after: true }],
+    },
+};
