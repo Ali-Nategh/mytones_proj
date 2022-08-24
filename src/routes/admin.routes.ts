66e8682cbd0b570b47462f0074432549a57a3638
Ali Nategh <alinategh005@gmail.com>
2022-08-24 21:56:00 +0430

    refactor: handled validation errors better

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,6 +4,7 @@ const router = require('express').Router();
 
 // Authentication function
 import authenticateToken from '../middlewares/authenticateToken.middleware';
+import validationMiddleware from '../middlewares/validateResults.middleware';
 
 
 /**
@@ -29,7 +30,7 @@ import authenticateToken from '../middlewares/authenticateToken.middleware';
  *          '403':
  *              description: Token invalid or expired
  */
-router.get('/getUsers', authenticateToken, adminGetAllUsers);
+router.get('/getUsers', authenticateToken, validationMiddleware, adminGetAllUsers);
 
 /**
  * @swagger
@@ -55,19 +56,13 @@ router.get('/getUsersTokenless', adminGetAllUsers); // For Testing
 
 /**
  * @swagger
- * /admin/getUsersTokenless:
+ * /admin/migratePrisma:
  *  get:
  *      tags: [Admin]
  *      summary: Get a list of users without token for tests
  *      responses:
  *          '200':
- *              description: The list of users
- *              content: 
- *                  apllication/json:
- *                      schema:
- *                          type: array
- *                          items: 
- *                              $ref: '#/components/schemas/User'
+ *              description: Updates the DataBase schema
  *          '400':
  *              description: Token not found
  *          '403':
