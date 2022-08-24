3a2fe21c996c37dc2393c34ad1909198aa343a81
Ali Nategh <alinategh005@gmail.com>
2022-08-24 21:55:35 +0430

    refactor: handled validation errors better

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,28 +1,23 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
 import { PrismaGetAllUsers } from "../repositories/user.repository";
 import { exec } from 'child_process';
 
 export async function adminGetAllUsers(req: Request, res: Response) {
-    // Validate and handle validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     const users = await PrismaGetAllUsers()
     return res.send(users);
 }
 
-export function adminMigratePrisma() {
+export function adminMigratePrisma(req: Request, res: Response) {
     exec('npx prisma migrate dev --name whateverName', (err, stdout, stderr) => {
         if (err) {
             //some err occurred    
             console.error(err)
+            return res.status(500).send('migration failed');
         } else {
             // the *entire* stdout and stderr (buffered)   
             console.log(`stdout: ${stdout}`);
             console.log(`stderr: ${stderr}`);
+            return res.status(200).send('migrated successfully');
         }
     });
 }
