14521e132c6f556df144bf92568ef22076ee2901
Ali Nategh <alinategh005@gmail.com>
2022-08-24 21:24:35 +0430

    refactor: added testing admin func

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { PrismaGetAllUsers } from "../repositories/user.repository";
+import { exec } from 'child_process';
 
 export async function adminGetAllUsers(req: Request, res: Response) {
     // Validate and handle validation errors
@@ -11,4 +12,17 @@ export async function adminGetAllUsers(req: Request, res: Response) {
 
     const users = await PrismaGetAllUsers()
     return res.send(users);
+}
+
+export function adminMigratePrisma() {
+    exec('npx prisma migrate dev --name whateverName', (err, stdout, stderr) => {
+        if (err) {
+            //some err occurred    
+            console.error(err)
+        } else {
+            // the *entire* stdout and stderr (buffered)   
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        }
+    });
 }
