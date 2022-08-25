365df07626a9f984ed0e243aed48cac81930470e
Ali Nategh <alinategh005@gmail.com>
2022-08-25 19:00:02 +0430

    refactor: clean up

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
-import { PrismaDeleteAllUsers } from "../repositories/user.repository";
 
 export async function getHome(req: Request, res: Response) {
     res.status(200).send("WELCOME TO THE HOME PAGE");
