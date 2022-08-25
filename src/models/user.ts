79a839c4d8254aecb9fed5ff4a6c5a71ba022eb6
Ali Nategh <alinategh005@gmail.com>
2022-08-25 19:00:18 +0430

    update: updated user entity

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,18 @@
 export default class User {
     id: any = undefined;
     username: string;
+    name: string;
+    lastname: string;
     password: string;
     email: string;
     age: any;
     role: string = "BASIC";
     refreshToken: string = "";
 
-    constructor(name: string, email: string, password: string, age?: number) {
-        this.username = name;
+    constructor(username: string, name: string, lastname: string, email: string, password: string, age?: number) {
+        this.username = username;
+        this.name = name;
+        this.lastname = lastname;
         this.password = password;
         this.email = email;
         this.age = age;
