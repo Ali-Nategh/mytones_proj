fe409a988ba91f7cbe667c8dedb0ac5e4862f907
Ali Nategh <alinategh005@gmail.com>
2022-08-27 00:49:19 +0430

    feat: added music related routes and documentations

--- src/routes/music.routes.ts
+++ src/routes/music.routes.ts
@@ -1,4 +1,7 @@
-import { addMusic, addPlaylist, addArtist, addAlbum } from "../controllers/music.controller";
+import {
+    addMusic, queryMusic, addPlaylist, queryPlaylist, addArtist,
+    queryArtist, addAlbum, queryAlbum, queryFavorites
+} from "../controllers/music.controller";
 
 
 const router = require('express').Router();
@@ -28,6 +31,29 @@ router.post('/addMusic', addMusic);
 
 /**
  * @swagger
+ * /music/queryMusic:
+ *  post:
+ *      tags: [Music]
+ *      summary: Get a list of songs
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/SongQuery'
+ *      responses:
+ *          '201': 
+ *              description: Song was successfully created
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '500': 
+ *              description: Something went wrong creating song
+ */
+router.post('/queryMusic', queryMusic);
+
+
+/**
+ * @swagger
  * /music/addArtist:
  *  post:
  *      tags: [Music]
@@ -47,6 +73,29 @@ router.post('/addMusic', addMusic);
  *              description: Something went wrong creating song
  */
 router.post('/addArtist', addArtist);
+
+/**
+ * @swagger
+ * /music/queryArtist:
+ *  post:
+ *      tags: [Music]
+ *      summary: Get a list of Artists
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ArtistQuery'
+ *      responses:
+ *          '201': 
+ *              description: Song was successfully created
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '500': 
+ *              description: Something went wrong creating song
+ */
+router.post('/queryArtist', queryArtist);
+
 
 /**
  * @swagger
@@ -72,6 +121,29 @@ router.post('/addPlaylist', addPlaylist);
 
 /**
  * @swagger
+ * /music/queryPlaylist:
+ *  post:
+ *      tags: [Music]
+ *      summary: Get a list of Artists
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/PlaylistQuery'
+ *      responses:
+ *          '201': 
+ *              description: Song was successfully created
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '500': 
+ *              description: Something went wrong creating song
+ */
+router.post('/queryPlaylist', queryPlaylist);
+
+
+/**
+ * @swagger
  * /music/addAlbum:
  *  post:
  *      tags: [Music]
@@ -91,6 +163,51 @@ router.post('/addPlaylist', addPlaylist);
  *              description: Something went wrong creating song
  */
 router.post('/addAlbum', addAlbum);
+
+/**
+ * @swagger
+ * /music/queryAlbum:
+ *  post:
+ *      tags: [Music]
+ *      summary: Get a list of Artists
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/AlbumQuery'
+ *      responses:
+ *          '201': 
+ *              description: Song was successfully created
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '500': 
+ *              description: Something went wrong creating song
+ */
+router.post('/queryAlbum', queryAlbum);
+
+
+/**
+ * @swagger
+ * /music/queryFavorites:
+ *  post:
+ *      tags: [Music]
+ *      summary: Get a list of Artists
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/FavoritesQuery'
+ *      responses:
+ *          '201': 
+ *              description: Song was successfully created
+ *          '400':
+ *              description: Bad Request, information invalid
+ *          '500': 
+ *              description: Something went wrong creating song
+ */
+router.post('/queryFavorites', queryFavorites);
 
 
 export default router;
