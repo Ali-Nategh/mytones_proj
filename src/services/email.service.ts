9eaa03c51680784b6d21e46c1d9765e30e5c58ed
Ali Nategh <alinategh005@gmail.com>
2022-08-27 01:20:40 +0430

    refactor: better email info

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -28,9 +28,9 @@ export const sendMail = async (params: { to: any; OTP: any; }) => {
           class="container"
           style="max-width: 90%; margin: auto; padding-top: 20px"
         >
-          <h2>Welcome to the club.</h2>
-          <h4>You are officially In after entering the password ✔</h4>
-          <p style="margin-bottom: 30px;">Pleas enter the sign up OTP to get started</p>
+          <h2>Welcome to our Site!.</h2>
+          <h4>You can listen to your favorite music right after entering the password ✔</h4>
+          <p style="margin-bottom: 30px;">Pleas enter the sign up Password to get started</p>
           <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${params.OTP}</h1>
      </div>
       `,
