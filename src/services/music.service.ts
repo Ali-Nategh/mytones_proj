42dffa108313db23ca2f48cc8065341912770ce8
Ali Nategh <alinategh005@gmail.com>
2022-08-26 21:47:24 +0430

    feat: added more music related services

--- src/services/music.service.ts
+++ src/services/music.service.ts
@@ -1,25 +1,33 @@
+import { Genres } from "@prisma/client";
 import { Request, Response } from "express";
 import { sendError } from "../errors/errorHandler";
 import { httpStatusCodes } from "../errors/httpStatusCodes";
-import { PrismaCreateSong, PrismaFindSongFilepath, PrismaFindSongISRC, PrismaFindSong, PrismaFindAlbum, PrismaCreateArtist } from "../repositories/music.repository";
+import { PrismaFindUser } from "../repositories/user.repository";
+import {
+    PrismaCreateSong, PrismaFindSongFilepath, PrismaFindSongISRC, PrismaFindSong, PrismaCreatePlaylist,
+    PrismaFindAlbum, PrismaCreateArtist, PrismaCreateAlbum, PrismaFindArtist,
+} from "../repositories/music.repository";
+
 
 export async function addMusicService(req: Request, res: Response) {
     const file_path = await PrismaFindSongFilepath(req.body.file_path);
     if (file_path) {
         return sendError(httpStatusCodes.BAD_REQUEST, "File Path Already Exists", res)
     }
-    if (req.body.ISRC) {
-        const isrc = await PrismaFindSongISRC(req.body.ISRC)
+    const ISRC = req.body.ISRC
+    if (ISRC) {
+        const isrc = await PrismaFindSongISRC(ISRC)
         if (isrc) return sendError(httpStatusCodes.BAD_REQUEST, "ISRC Code Already Exists", res)
     }
 
     const song = await PrismaCreateSong(req.body.song_name, req.body.file_path, req.body.artist_id, req.body.duration
-        , req.body.album_id, req.body.publisher, req.body.ISRC, req.body.copyright_info
+        , req.body.album_id, req.body.publisher, ISRC, req.body.copyright_info
         , req.body.release_date, req.body.genres, req.body.producers, req.body.writers, req.body.engineers)
     console.log(song);
 
     return res.status(201).send("Song Created Successfully");
 }
+
 
 export async function addArtistService(req: Request, res: Response) {
     let noErrors = true;
@@ -45,8 +53,67 @@ export async function addArtistService(req: Request, res: Response) {
         }
     }
     if (noErrors) {
-        const artist = await PrismaCreateArtist(req.body.artist_name, req.body.albums_id, req.body.songs_id);
+        const artist = await PrismaCreateArtist(req.body.artist_name, req.body.albums_id, songs);
         console.log(artist);
         return res.status(201).send("Artist Created Successfully");
     }
+}
+
+
+export async function addAlbumService(req: Request, res: Response) {
+    let noErrors = true;
+    const artistid = req.body.artist_id
+    const songs = req.body.songs_id;
+    const genres = req.body.genres;
+
+    const artist = await PrismaFindArtist(artistid)
+    if (!artist) return sendError(httpStatusCodes.NOT_FOUND, `Artist ID (${artistid}) Not Found`, res)
+    if (songs) {
+        for (let i = 0; i < songs.length; i++) {
+            const song = await PrismaFindSong(songs[i])
+            if (!song) {
+                noErrors = false;
+                return sendError(httpStatusCodes.NOT_FOUND, `Song ID (${songs[i]}) Not Found`, res)
+            }
+        }
+    }
+    if (genres && noErrors) {
+        for (let i = 0; i < genres.length; i++) {
+            if (!(genres[i] in Genres)) {
+                noErrors = false;
+                return sendError(httpStatusCodes.NOT_FOUND, `Genre (${genres[i]}) Not Found`, res)
+            }
+        }
+    }
+    if (noErrors) {
+        const album = await PrismaCreateAlbum(req.body.album_name, req.body.artist_id, req.body.release_date, songs, genres)
+        console.log(album);
+        return res.status(201).send("Album Created Successfully");
+    }
+}
+
+
+export async function addPlaylistService(req: Request, res: Response) {
+    const user_id = req.body.user_id
+    const user = await PrismaFindUser(user_id)
+    if (!user) return sendError(httpStatusCodes.NOT_FOUND, `User (${user_id}) Not Found`, res)
+
+    let noErrors = true;
+    const songs = req.body.songs_id;
+    if (songs) {
+        for (let i = 0; i < songs.length; i++) {
+            const song = await PrismaFindSong(songs[i])
+            if (!song) {
+                noErrors = false;
+                return sendError(httpStatusCodes.NOT_FOUND, `Song ID (${songs[i]}) Not Found`, res)
+            }
+        }
+    }
+
+    if (noErrors) {
+        const playlist = await PrismaCreatePlaylist(req.body.playlist_name, user_id, songs)
+        console.log(playlist)
+    }
+
+    return res.status(201).send("Playlist Created Successfully");
 }
