c8a6c582f0160c6606aa0899ef92d570f3f7e310
Ali Nategh <alinategh005@gmail.com>
2022-09-02 14:39:21 +0430

    feat: a toJson func for Big Int support

--- src/utils/toJson.ts
+++ src/utils/toJson.ts
@@ -1,4 +1,4 @@
-// Does JSON.stringify, with support for BigInt:
+// Does JSON.stringify, with support for BigInt 
 export function toJson(data: any) {
     return JSON.parse(JSON.stringify(data, (_, v) => typeof v === 'bigint' ? v.toString() : v));
 }
