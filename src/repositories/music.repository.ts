6778fd6732aae95c77159e91b395230b23cb016c
Ali Nategh <alinategh005@gmail.com>
2022-09-02 14:33:55 +0430

    feat: added update for music repositories

--- src/repositories/music.repository.ts
+++ src/repositories/music.repository.ts
@@ -2,6 +2,7 @@
 import { Genres, PrismaClient, Type } from '@prisma/client';
 const prisma = new PrismaClient();
 
+import { seperator } from '../utils/seperator';
 
 export async function PrismaCreateSong(song_name: string, file_path: string, artist_id: string, duration: string
     , album_id?: string, publisher?: string, ISRC?: string, copyright_info?: string, release_date?: string
@@ -12,6 +13,25 @@ export async function PrismaCreateSong(song_name: string, file_path: string, art
             file_path: file_path,
             artist_id: artist_id,
             duration: duration,
+            album_id: album_id,
+            publisher: publisher,
+            ISRC: ISRC,
+            copyright_info: copyright_info,
+            release_date: release_date,
+            genres: genres,
+            producers: producers,
+            writers: writers,
+            engineers: engineers,
+        }
+    });
+    return song;
+}
+export async function PrismaUpdateSong(song_id: string, album_id?: string, publisher?: string, ISRC?: string
+    , copyright_info?: string, release_date?: string, genres?: Genres[], producers?: string[]
+    , writers?: string[], engineers?: string[]) {
+    const song = await prisma.song.update({
+        where: { id: song_id },
+        data: {
             album_id: album_id,
             publisher: publisher,
             ISRC: ISRC,
@@ -61,6 +81,18 @@ export async function PrismaCreateArtist(artist_name: string, albums_id?: string
     });
     return artist;
 }
+export async function PrismaUpdateArtist(artist_id: string, albums_id?: string[], songs_id?: string[]) {
+    const artist = await prisma.artist.update({
+        where: {
+            id: artist_id
+        },
+        data: {
+            albums_id: albums_id,
+            songs_id: songs_id,
+        },
+    });
+    return artist;
+}
 export async function PrismaFindArtist(artist_id: string) {
     const artist = await prisma.artist.findUnique({
         where: { id: artist_id }
@@ -88,6 +120,18 @@ export async function PrismaCreateAlbum(album_name: string, artist_id: string, r
     });
     return album;
 }
+export async function PrismaUpdateAlbum(album_id: string, songs_id?: string[], genres?: Genres[]) {
+    const album = await prisma.album.update({
+        where: {
+            id: album_id
+        },
+        data: {
+            songs_id: songs_id,
+            genres: genres,
+        },
+    });
+    return album;
+}
 export async function PrismaFindAlbum(album_id: string) {
     const album = await prisma.album.findUnique({
         where: { id: album_id }
@@ -109,6 +153,25 @@ export async function PrismaCreatePlaylist(playlist_name: string, user_id: strin
             user_id: user_id,
             songs_id: songs_id
         },
+    });
+    return playlist;
+}
+export async function PrismaUpdatePlaylist(playlist_id: string, user_id: string, playlist_name?: string, songs_id?: string[]) {
+    const playlist = await prisma.playlist.update({
+        where: {
+            id: playlist_id,
+            user_id: user_id,
+        },
+        data: {
+            name: playlist_name,
+            songs_id: songs_id
+        },
+    });
+    return playlist;
+}
+export async function PrismaFindPlaylist(playlist_id: string) {
+    const playlist = await prisma.playlist.findUnique({
+        where: { id: playlist_id }
     });
     return playlist;
 }
@@ -147,6 +210,17 @@ export async function PrismaCreateUserFavorites(user_id: string) {
     });
     return [favoriteSongs, favoriteArtists, favoriteAlbums, downloads]
 }
+export async function PrismaUpdateFavorites(user_id: string, type: Type, songs_id?: string[]) {
+    const favorites = await prisma.favorites.update({
+        where: {
+            user_id_type: { user_id, type },
+        },
+        data: {
+            songs_id: songs_id
+        }
+    });
+    return favorites;
+}
 export async function PrismaFavoritesQuery(user_id: string, type: Type) {
     const favorites = await prisma.favorites.findMany({
         where: { AND: [{ user_id: user_id }, { type: type }] }
@@ -161,5 +235,10 @@ export async function PrismaGetAllMusic() {
     const artists = await prisma.artist.findMany({});
     const playlists = await prisma.playlist.findMany({});
     const favorites = await prisma.favorites.findMany({});
-    return [songs, albums, artists, playlists, favorites];
+    const genres = await prisma.genre.findMany({});
+    return [
+        seperator("SONGS"), songs, seperator("ALBUMS"), albums, seperator("ARTISTS"), artists,
+        seperator("PLAYLISTS"), playlists, seperator("FAVORITES"), favorites, seperator("GENRES"), genres,
+        seperator("")
+    ];
 }
