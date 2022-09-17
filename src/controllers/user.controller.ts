11ff9b1bb34a3f529c87a6b8dd5b22ac30bb6332
Ali Nategh <alinategh005@gmail.com>
2022-09-17 21:02:16 +0430

    feat: moved error handling and improved it from services to controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,26 +1,74 @@
 import { loginUserService, signUpUserService, logoutUserService, refreshUserTokenService } from "../services/user.service";
 import { resendEmailService, verifyEmailService } from "../services/email.service";
+import { sendOperationalError, logError, sendError } from "../errors/errorHandler";
 import { Request, Response, NextFunction } from "express";
+import { httpStatus } from "../errors/httpStatusCodes";
+import BaseError from "../errors/baseError";
 
 
 
 export async function signUpUser(req: Request, res: Response) {
-    signUpUserService(req, res);
+    try {
+        await signUpUserService(req, res);
+    } catch (error: BaseError | any) {
+        if (error.name === "AGE_ERROR") {
+            return sendOperationalError(error, res)
+        } else if (error.name === "EMAIL_ERROR") {
+            return sendOperationalError(error, res)
+        } else if (error.name === "INTERNAL_SERVER_ERROR") {
+            return sendOperationalError(error, res)
+        } else {
+            logError(error)
+            return sendError(httpStatus.INTERNAL_SERVER_ERROR, error, res)
+        }
+    }
 };
 
 
 export async function loginUser(req: Request, res: Response) {
-    loginUserService(req, res);
+    try {
+        await loginUserService(req, res);
+    } catch (error: BaseError | any) {
+        if (error.name === "EMAIL_ERROR") {
+            return sendOperationalError(error, res)
+        } else if (error.name === "AUTHORITY_ERROR") {
+            return sendOperationalError(error, res)
+        } else if (error.name === "INTERNAL_SERVER_ERROR") {
+            logError(error)
+            return sendOperationalError(error, res)
+        } else {
+            logError(error)
+            return sendError(httpStatus.INTERNAL_SERVER_ERROR, error, res)
+        }
+    }
 }
 
 
 export async function logoutUser(req: Request, res: Response) {
-    logoutUserService(req, res);
+    try {
+        await logoutUserService(req, res);
+    } catch (error: BaseError | any) {
+        if (error.name === "TOKEN_ERROR") {
+            return sendOperationalError(error, res)
+        } else {
+            logError(error)
+            return sendError(httpStatus.INTERNAL_SERVER_ERROR, error, res)
+        }
+    }
 }
 
 
 export async function refreshUserToken(req: Request, res: Response) {
-    refreshUserTokenService(req, res);
+    try {
+        await refreshUserTokenService(req, res);
+    } catch (error: BaseError | any) {
+        if (error.name === "TOKEN_ERROR") {
+            return sendOperationalError(error, res)
+        } else {
+            logError(error)
+            return sendError(httpStatus.INTERNAL_SERVER_ERROR, error, res)
+        }
+    }
 }
 
 export async function validateUserOtp(req: Request, res: Response) {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -11,6 +11,11 @@ import { sendMail } from '../services/email.service';
 import generateOTP from '../services/otp.service';
 import { sendError } from "../errors/errorHandler";
 import { authorizePass } from "../utils/authPass";
+import Api400Error from "../errors/api400Error";
+import Api401Error from "../errors/api401Error";
+import Api403Error from "../errors/api403Error";
+import Api404Error from "../errors/api404Error";
+import Api500Error from "../errors/api404Error";
 import hashPass from "../utils/hashPass";
 import User from "../models/user";
 
@@ -20,7 +25,7 @@ export async function signUpUserService(req: Request, res: Response) {
 
     if (req.body.age) {
         const userAge = parseInt(req.body.age);
-        if (userAge < 0 || userAge == NaN) return sendError(httpStatus.BAD_REQUEST, "Age Is Invalid", res)
+        if (userAge < 0 || userAge == NaN) throw new Api400Error("Age Is Invalid", "AGE_ERROR")
         req.body.age = userAge
     }
 
@@ -29,16 +34,16 @@ export async function signUpUserService(req: Request, res: Response) {
     // Adding user to Database
     try {
         const emailExisting = await PrismaFindEmail(user.email)
-        if (emailExisting) return sendError(httpStatus.BAD_REQUEST, "Email Already Exists", res)
+        if (emailExisting) throw new Api404Error("Email Already Exists", "EMAIL_ERROR")
 
         const user_data = await createUser(user);
         console.log(user_data);
 
         const user_id = await PrismaFindUserByEmail(user.email)
-        if (!user_id) return sendError(httpStatus.INTERNAL_SERVER_ERROR, "User Not Found", res)
+        if (!user_id) throw new Api500Error("User Not Found", "INTERNAL_SERVER_ERROR")
 
         const userOTP = await RedisFindOTP(user_id.id)
-        if (!userOTP) return sendError(httpStatus.INTERNAL_SERVER_ERROR, "Something went wrong sending password to email", res)
+        if (!userOTP) throw new Api500Error("Something went wrong sending password to email", "INTERNAL_SERVER_ERROR")
 
         sendMail({ to: user.email, OTP: userOTP })
         return res.status(201).send("User Created Successfully");
@@ -59,31 +64,35 @@ export async function signUpUserService(req: Request, res: Response) {
         })
     };
 };
+export async function createUser(user: User) {
+    const user_data = await PrismaUserCreation(user.username, user.name, user.lastname, user.email, user.password, generateOTP(), user?.age);
+    return user_data;
+}
 
 
 export async function loginUserService(req: Request, res: Response) {
     let user = await PrismaFindUserByEmail(req.body.email);
     if (user == null) {
-        return sendError(httpStatus.BAD_REQUEST, 'Cannot find user', res)
+        throw new Api400Error('Cannot find user', "EMAIL_ERROR")
     };
 
     const userEmail = await PrismaFindEmail(req.body.email)
-    if (!userEmail?.verified) return sendError(httpStatus.UNAUTHORIZED, "Email is not active", res)
+    if (!userEmail?.verified) throw new Api401Error("Email is not active", "EMAIL_ERROR")
 
     try {
         if (await authorizePass(req.body.password, user.password)) {
             const accessToken = jwtAccessGen(user.id)
             const refresh_token = jwtRefreshGen(user.id)
-            const create_token = await RedisCreateRefreshToken(refresh_token, user.id)
+            const redis_create = await RedisCreateRefreshToken(refresh_token, user.id)
             console.log(["Access", accessToken, "Refresh", refresh_token]);
 
             return res.status(200).json({ accessToken: accessToken, refresh_token: refresh_token });
         } else {
-            return sendError(httpStatus.UNAUTHORIZED, "Not Allowed", res)
+            throw new Api403Error("Not Allowed", "AUTHORITY_ERROR")
         }
     } catch (err) {
         console.error(err);
-        return sendError(httpStatus.INTERNAL_SERVER_ERROR, 'Something went wrong in user login', res)
+        throw new Api500Error('Something went wrong in user login', "INTERNAL_SERVER_ERROR")
     };
 }
 
@@ -91,12 +100,12 @@ export async function loginUserService(req: Request, res: Response) {
 export async function logoutUserService(req: Request, res: Response) {
     const sentRefreshToken = req.body.token
 
-    jwt.verify(sentRefreshToken, process.env.REFRESH_TOKEN_SECRET as string, async (err: VerifyErrors | null, user_id: any) => {
+    await jwt.verify(sentRefreshToken, process.env.REFRESH_TOKEN_SECRET as string, async (err: VerifyErrors | null, user_id: any) => {
         if (err) {
-            return sendError(403, 'Token is invalid or has expired, please login again', res)
+            throw new Api403Error('Token is invalid or has expired, please login again', "TOKEN_ERROR")
         }
         const refreshtoken = await RedisFindRefreshToken(sentRefreshToken)
-        if (!refreshtoken) return sendError(httpStatus.UNAUTHORIZED, "Refresh token not found", res)
+        if (!refreshtoken) throw new Api401Error("Refresh token not found", "TOKEN_ERROR")
 
         await RedisDeactivateRefreshToken(sentRefreshToken)
         return res.status(200).send("Logged out successfully");
@@ -107,20 +116,13 @@ export async function logoutUserService(req: Request, res: Response) {
 export async function refreshUserTokenService(req: Request, res: Response) {
     const sentRefreshToken = req.body.token;
 
-    if (sentRefreshToken == null) return sendError(httpStatus.BAD_REQUEST, "Please include a refresh token", res)
-
-    jwt.verify(sentRefreshToken, process.env.REFRESH_TOKEN_SECRET as string, async (err: VerifyErrors | null, user_id: any) => {
+    await jwt.verify(sentRefreshToken, process.env.REFRESH_TOKEN_SECRET as string, async (err: VerifyErrors | null, user_id: any) => {
         if (err) {
-            return sendError(403, 'Token is invalid or has expired, please login again', res)
+            throw new Api403Error('Token is invalid or has expired, please login again', "TOKEN_ERROR")
         }
         const refreshtoken = await RedisFindRefreshToken(sentRefreshToken)
-        if (!refreshtoken) return sendError(httpStatus.UNAUTHORIZED, "Refresh token is not active, please login", res)
+        if (!refreshtoken) throw new Api401Error("Refresh token is not active, please login", "TOKEN_ERROR")
 
         return refreshToken(sentRefreshToken, res);
     });
-}
-
-export async function createUser(user: User) {
-    const user_data = await PrismaUserCreation(user.username, user.name, user.lastname, user.email, user.password, generateOTP(), user?.age);
-    return user_data;
 }
